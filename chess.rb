9979e6d907c3c2d674d883ab04f65951672e42f1
martin <mocon@DESKTOP-1GQAV5L>
2021-10-09 13:44:39 +0200

    Start Gemfile

--- chess.rb
+++ chess.rb
@@ -1,1 +1,21 @@
+
+
 p "Chess init"
+
+class Game
+
+  def initialize()
+    # @board = Board.new
+    @player1 = Player.new
+    @player2 = Player.new
+  end
+
+end
+
+class Player
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
