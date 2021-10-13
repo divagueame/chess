6042b5e5b94a1721386ceda283e41b99cec82042
martin <mocon@DESKTOP-1GQAV5L>
2021-10-13 09:14:48 +0200

    Add Pawn Class

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,39 +1,141 @@
 # require 'rspec/autorun'
 
+class Player
+  def initialize(colorPlayer="white", humanPlayer = true)
+    @colorPlayer = colorPlayer
+    @humanPlayer = humanPlayer
+  end
+
+  def movePiece
+
+  end
+end
+
+# Module Pieces 
+  # include Pawn
+  # PIECES  = {
+  #   whitepawn: '♟', 
+  #   whitehorse: '♞',
+  #   whitebishop: '♝',
+  #   whitequeen: '♛',
+  #   whiteking: '♚',
+  #   whiterook: '♜',
+  #   blackpawn: '♙',
+  #   blackhorse: '♘',
+  #   blackbishop: '♗',
+  #   blackqueen: '♕',
+  #   blackking: '♔',
+  #   blackrook: '♖'
+  # }
 
 
+  class Pawn
+    attr_accessor :allowedMoves, :row, :col, :pieceStr
+    def initialize(row,col,color,moved = false)
+      @row = row
+      @col = col
+      @color = color
+      @moved = moved
+      color=="white"? @pieceStr = '♟': @pieceStr = '♙'
+      @allowedMoves = [[1,0],[2,0]]
+    end
+  end
+
+  class Rook
+    attr_accessor :allowedMoves, :row, :col, :pieceStr
+    def initialize(row,col,color,moved = false)
+      @row = row
+      @col = col
+      @color = color
+      @moved = moved
+      color=="white"? @pieceStr = '♜': @pieceStr = '♖'
+      @allowedMoves = getRookMoves
+    end
+
+    def getRookMoves
+      arr = []
+      # Horizontal positive
+
+      # Horizontal negative
+
+      # Vertical positive
+
+      # Vertical negative
+      arr
+    end
+  end
 
 class Board
+  attr_accessor :currentBoard
   
-  PIECES  = {
-    whitepawn: '♟', 
-    whitehorse: '♞',
-    whitebishop: '♝',
-    whitequeen: '♛',
-    whiteking: '♚',
-    whitetower: '♜',
-    blackpawn: '♙',
-    blackhorse: '♘',
-    blackbishop: '♗',
-    blackqueen: '♕',
-    blackking: '♔',
-    blacktower: '♖'
-  }
+  def initialize
 
-attr_accessor :currentBoard
-    def initialize
-      @currentBoard = [
-        ['♖','♘','♗','♕','♔','♗','♘','♖'],
-        ['♙','♙','♙','♙','♙','♙','♙','♙'],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        ['♟','♟','♟','♟','♟','♟','♟','♟'],
-        ['♜','♞','♝','♛','♔','♝','♞','♜'],
+      @piecesRoot =[
+        # White pieces
+        Pawn.new(6,0,"white"),
+        Pawn.new(6,1,"white"),
+        Pawn.new(6,2,"white"),
+        Pawn.new(6,3,"white"),
+        Pawn.new(6,4,"white"),
+        Pawn.new(6,5,"white"),
+        Pawn.new(6,6,"white"),
+        Pawn.new(6,7,"white"),
+        Rook.new(7,0,"white"),
+        Rook.new(7,7,"white"),
+        
+        # Black pieces
+        Pawn.new(1,0,"Black"),
+        Pawn.new(1,1,"Black"),
+        Pawn.new(1,2,"Black"),
+        Pawn.new(1,3,"Black"),
+        Pawn.new(1,4,"Black"),
+        Pawn.new(1,5,"Black"),
+        Pawn.new(1,6,"Black"),
+        Pawn.new(1,7,"Black"),
+        Rook.new(0,0,"black"),
+        Rook.new(0,7,"black")
+      ]
+
+      @currentBoard = renderBoardArray
+      # @currentBoard = [
+      #   ['♖','♘','♗','♕','♔','♗','♘','♖'], #0
+      #   ['♙','♙','♙','♙','♙','♙','♙','♙'], #1
+      #   [0,0,0,0,0,0,0,0],                        #2  
+      #   [0,0,0,0,0,0,0,0],                        #3
+      #   [0,0,0,0,0,0,0,0],                        #4
+      #   [0,0,0,0,0,0,0,0],                        #5
+      #   ['♟','♟','♟','♟','♟','♟','♟','♟'], #6
+      #   ['♜','♞','♝','♛','♚','♝','♞','♜']  #7
+      #   # 0    1    2    3    4    5    6   7
+      # ]
+    end
+
+  def renderBoardArray
+    currentBoardArray = [
+        [0,0,0,0,0,0,0,0],                         #0
+        [0,0,0,0,0,0,0,0],                         #1
+        [0,0,0,0,0,0,0,0],                         #2  
+        [0,0,0,0,0,0,0,0],                         #3
+        [0,0,0,0,0,0,0,0],                         #4
+        [0,0,0,0,0,0,0,0],                         #5
+        [0,0,0,0,0,0,0,0],                         #6
+        [0,0,0,0,0,0,0,0]                          #7
+       # 0 1 2 3 4 5 6 7
       ]
       
-    end
+      @piecesRoot.each do |piece|
+        # p piece.row
+        # p piece.col
+        # p piece.pieceStr
+        thisCol = piece.col
+        thisRow = piece.row
+        # p thisCol
+        # p thisRow
+        currentBoardArray[thisRow][thisCol] = piece.pieceStr
+      end
+      currentBoardArray
+
+  end
 
   def drawSquare(color,piece=0)
     if piece == 0 || piece == "0" then piece = " " end
@@ -65,15 +167,51 @@ attr_accessor :currentBoard
     print "\n"
   end
 
-  
-  
+  def emptySquare?(col,row)
+    @currentBoard[row][col] == 0 ? true : false
+  end
+
+  def getCurrentPiece(col,row)
+    return @currentBoard[row][col]
+  end
+
+  def emptyOriginSquare(col,row)
+    return nil if @currentBoard[row][col] = 0
+    @currentBoard[row][col] = 0
+  end
+
+  def updateTargetSquare(col,row,movingPiece)
+    @currentBoard[row][col] = movingPiece
+  end
+
+  def updateMoveOnBoard(origin,target)
+    movingPiece = getCurrentPiece(origin[0],origin[1])
+    emptyOriginSquare(origin[0],origin[1])
+    updateTargetSquare(target[0],target[1],movingPiece)
+  end
+
+  def isInsideOfBoard(col,row)
+    return false if col<0 || row< 0 || col > 7 || row > 7
+    true
+  end
+
+  def isValidMove(origin,target,piece)
+
+  end
+
 end
 game = Board.new
-# print game.currentBoard
-# print game.drawSquare("white",Board::PIECES[:whitepawn])
+# game.renderBoardArray
+
 game.drawBoard
-# p game.currentBoard
-# miarr = ['♙','♙','♙','♙','♙','0',0,'♙']
-# game.drawRow(miarr,1)
-# print  "\n"
-# game.drawRow(miarr,0)
+# p Board.ancestors
+# pawn = Pawn.new(0,1,"white")
+# pawn2 = Pawn.new(0,1,"black")
+# p pawn
+# p pawn.allowedMoves
+# pawn.move()
+# p game.ancestors
+
+# game.updateMoveOnBoard([1,0],[2,2])
+
+# game.drawBoard
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,13 +1,72 @@
 require 'chess'
 
 describe Board do
-  context "draws empty squares properly" do
-    it "shows an empty white square" do
-      emptyBoard = Board.new
-      actual = emptyBoard.drawSquare("white")
-      expect(actual).to eq('\\u001b[46;1m   \\e[0m')
+  let(:startingBoard) {Board.new}
+  context "#emptySquare checks empty squares properly" do
+    # let(:emptySquareBoard) {Board.new}
+    it "returns true if there's no piece in the given square" do
+      actual = startingBoard.emptySquare?(3,3)
+      expect(actual).to eq(true)
+    end
+    it "returns false if there's a piece in it" do
+      actual = startingBoard.emptySquare?(0,0)
+      expect(actual).to eq(false)
     end
     
-    
+    context "#getCurrentPiece returns the correct piece" do
+      it 'returns the white king from 4,0' do
+        actual = startingBoard.getCurrentPiece(4,7)
+        whiteking = '♚'
+        expect(actual).to eq(whiteking)
+      end
+      it "returns 0 if it's empty square" do
+        actual = startingBoard.getCurrentPiece(3,3)
+        expect(actual).to eq(0)
+      end
+    end
+
+    context "#emptyOriginSquare" do
+      it "empties the origin square" do
+        startingBoard.emptyOriginSquare(0,6)
+        actual = startingBoard.currentBoard[6][0]
+        expect(actual).to eq(0)
+      end
+      it "returns nil if the square is already empty" do
+        startingBoard.emptyOriginSquare(3,3)
+        actual = startingBoard.getCurrentPiece(3,3)
+        expect(actual).to eq(0)
+      end
+    end
+
+    context "#updateTargetSquare moves the piece to its target" do
+      it 'updates the new value of the target square' do
+        movingPiece = '♟'
+        startingBoard.updateTargetSquare(6,5,movingPiece)
+        actual = startingBoard.currentBoard[5][6]
+        expect(actual).to eq(movingPiece)
+      end
+    end
+
+    context "#updateMoveOnBoard" do
+      it 'advances pawn on the array @currentBoard' do
+        origin = [0,6]
+        target = [0,5]
+        startingBoard.updateMoveOnBoard(origin,target)
+        actual = startingBoard.currentBoard
+        updatedBoard= 
+        [
+          ['♖','♘','♗','♕','♔','♗','♘','♖'], #0
+          ['♙','♙','♙','♙','♙','♙','♙','♙'], #1
+          [0,0,0,0,0,0,0,0],                        #2  
+          [0,0,0,0,0,0,0,0],                        #3
+          [0,0,0,0,0,0,0,0],                        #4
+          ['♟',0,0,0,0,0,0,0],                        #5
+          [0,'♟','♟','♟','♟','♟','♟','♟'], #6
+          ['♜','♞','♝','♛','♚','♝','♞','♜']  #7
+          # 0    1    2    3    4    5    6   7
+        ]
+        expect(actual).to eq(updatedBoard)
+      end
+    end
   end
 end
